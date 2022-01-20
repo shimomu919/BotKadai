8e99358d1a5e459da53ad2f04b6b07f0c5819749
test <kennkyuusituyou.mail@gmail.com>
2022-01-20 16:32:06 +0900

    CPU's hand

--- Computer.java
+++ Computer.java
@@ -2,9 +2,8 @@ import java.util.Random;
 
 public class Computer {
 	public static int decidesComputerHand() {
-		//課�?3 Computerの手�?�決定とそ�?�表示
-		//Randomメソ�?ドを使って1~3の数値を返す
+		int hand = new Random().nextInt(3) + 1;
 
-
+		return hand;
 	}
 }
