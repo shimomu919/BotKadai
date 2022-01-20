83e5586f07f21275c56bac928cd157a0436d3a81
kznk7 <56796881+kznk7@users.noreply.github.com>
2022-01-20 16:34:12 +0900

    Computer.java

--- Computer.java
+++ Computer.java
@@ -1,9 +1,11 @@
 import java.util.Random;
 
 public class Computer {
-	public static int decidesComputerHand() {
-		int hand = new Random().nextInt(3) + 1;
-
-		return hand;
-	}
+    public static int decidesComputerHand() {
+        //課題3 Computerの手の決定とその表示
+        //Randomメソッドを使って1~3の数値を返す
+        Random random = new Random();
+        int rnd = random.nextInt(3)+1;
+        return rnd;
+    }
 }
