b49fad641ee1fe1e5dd0ab9095cfbc19b1a5376a
kznk7 <56796881+kznk7@users.noreply.github.com>
2022-01-20 16:34:11 +0900

    Player.java

--- Player.java
+++ Player.java
@@ -5,16 +5,18 @@ public class Player {
     int playerHand;
     Scanner scanner = new Scanner(System.in);
 
+    //課題2(Bot) Playerの手の決定とその表示
+    //1~3以外の入力に対してエラー文の表示および再入力させる処理
     while(true) {
       try {
         playerHand = Integer.parseInt(scanner.next());
         if (playerHand < 1 || playerHand > 3) {
-          System.out.println("1~3�̐��l����͂��Ă��������D");
+          System.out.println("1~3の数値を入力して下さい。");
           continue;
         }
         break;
       } catch(Exception e) {
-        System.out.println("1~3�̐��l����͂��Ă��������D");
+        System.out.println("1~3の数値を入力して下さい。");
       }
     }
     scanner.close();
